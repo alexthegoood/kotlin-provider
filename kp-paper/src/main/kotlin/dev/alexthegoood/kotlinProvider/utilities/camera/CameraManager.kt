fa1224f663b47d14acfd021322cc912fcd61af08
alexthegoood <mail.alexthegoood@gmail.com>
2026-05-24 22:34:44 +0300

    use class instead object...

--- kp-paper/src/main/kotlin/dev/alexthegoood/kotlinProvider/utilities/camera/CameraManager.kt
+++ kp-paper/src/main/kotlin/dev/alexthegoood/kotlinProvider/utilities/camera/CameraManager.kt
@@ -1,27 +1,26 @@
 package dev.alexthegoood.kotlinProvider.utilities.camera
 
 import dev.alexthegoood.kotlinProvider.utilities.nms.player.serverPlayer
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import net.minecraft.network.protocol.game.ClientboundPlayerRotationPacket
 import org.bukkit.entity.Player
 import kotlin.time.Duration.Companion.milliseconds
 
-object CameraManager {
+class CameraManager(private val player: Player) {
 
     private val scope = CoroutineScope(Dispatchers.Default)
 
-    fun rotate(player: Player, yaw: Float, pitch: Float, packetsPerTick: Int = 1) = scope.launch {
+    fun rotate(yaw: Float, pitch: Float, packetsPerTick: Int = 1): Job {
         val packetsPerTick = packetsPerTick.coerceAtLeast(1)
         val rotationPerTick = ClientboundPlayerRotationPacket(yaw/packetsPerTick, true, pitch/packetsPerTick, true)
         val duration = (50/packetsPerTick).milliseconds
         val connection = player.serverPlayer.connection
-        repeat(packetsPerTick) {
-            if (!player.isOnline) return@repeat
-            connection.send(rotationPerTick)
-            if (packetsPerTick > 1) delay(duration)
+        return scope.launch {
+            repeat(packetsPerTick) {
+                if (!player.isOnline) return@launch
+                connection.send(rotationPerTick)
+                if (packetsPerTick > 1) delay(duration)
+            }
         }
     }
 
