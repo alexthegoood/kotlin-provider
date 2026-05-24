8ef93876f9d734c83d03db7d3d86e12f32bee552
alexthegoood <mail.alexthegoood@gmail.com>
2026-05-24 17:00:19 +0300

    add CameraManager

---
+++ kp-paper/src/main/kotlin/dev/alexthegoood/kotlinProvider/utilities/camera/CameraManager.kt
@@ -0,0 +1,46 @@
+package dev.alexthegoood.kotlinProvider.utilities.camera
+
+import dev.alexthegoood.kotlinProvider.utilities.nms.player.serverPlayer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import net.minecraft.network.protocol.game.ClientboundPlayerRotationPacket
+import org.bukkit.entity.Player
+import kotlin.time.Duration.Companion.nanoseconds
+
+class CameraManager(val player: Player) {
+    companion object {
+
+        private val scope = CoroutineScope(Dispatchers.Default)
+
+        // it is not working
+//        fun visionType(player: Player, visionType: VisionTypes) {
+//            val entity = visionType.entityType
+//                .create(player.serverLevel, null, player.blockPos, EntitySpawnReason.COMMAND, false, false) ?: return
+//            val addEntity = ClientboundAddEntityPacket(entity, 0, player.blockPos)
+//            val setCamera = ClientboundSetCameraPacket(entity)
+//            val removeEntity = ClientboundRemoveEntitiesPacket(entity.id)
+//            player.serverPlayer.connection.send(addEntity)
+//            player.serverPlayer.connection.send(setCamera)
+//            player.serverPlayer.connection.send(removeEntity)
+//        }
+
+        fun rotate(player: Player, yaw: Float, pitch: Float, packetsPerMS: Int = 1) {
+            scope.launch {
+                val PPMS = packetsPerMS.coerceAtLeast(1)
+                val rotationPerMS = ClientboundPlayerRotationPacket(yaw/PPMS, true, pitch/PPMS, true)
+                val duration = (50/PPMS).nanoseconds
+                val connection = player.serverPlayer.connection
+                repeat(PPMS) {
+                    connection.send(rotationPerMS)
+                    delay(duration)
+                }
+            }
+        }
+
+    }
+
+    fun rotate(yaw: Float, pitch: Float, packetsPerMS: Int = 1) =
+        rotate(player, yaw, pitch, packetsPerMS)
+}
---
+++ kp-paper/src/main/kotlin/dev/alexthegoood/kotlinProvider/utilities/camera/VisionTypes.kt
@@ -0,0 +1,9 @@
+package dev.alexthegoood.kotlinProvider.utilities.camera
+
+import net.minecraft.world.entity.EntityType
+
+//enum class VisionTypes(val entityType: EntityType<*>) {
+//    ENDERMAN(EntityType.ENDERMAN),
+//    CREEPER(EntityType.CREEPER),
+//    SPIDER(EntityType.SPIDER);
+//}
